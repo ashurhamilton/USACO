970e05a761d35802eacbc023780ef6a649093df3
Ashur <ashurhamilton2022@gmail.com>
2023-06-02 18:59:41 -0700

    finished

--- cowherding.py
+++ cowherding.py
@@ -2,23 +2,29 @@ x = int(input())
 y = int(input())
 z = int(input())
 
-unsorted = []
-unsorted.append(x)
-unsorted.append(y)
-unsorted.append(z)
-
-sorted = unsorted.sort()
-
+sorted = []
+sorted.append(x)
+sorted.append(y)
+sorted.append(z)
+sorted.sort()
 a = sorted[1] - sorted[0]
 b = sorted[2] - sorted[1]
-
+print(a, b)
 if a<=b:
     if a<=2:
-        min = 1
+        small = 1
     else:
-        min = 2
-elif b>a:
+        small = 2
+else:
     if b<=2:
-        min = 1
+        small = 1
     else:
-        min = 2
+        small = 2
+
+if a>=b:
+    large = a-1
+if b>a:
+    large = b-1
+
+print(small)
+print(large)
