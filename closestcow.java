0e822f3fb7d686b865fe7e5c80ca2de941fb4305
Ashur <ashurhamilton2022@gmail.com>
2024-06-23 18:29:13 -0700

    wow

---
+++ closestcow.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class ClosestCow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int currentLine = 0;
+
+        while (scanner.hasNextLine()) {
+            currentLine += 1;  
+            String line = scanner.nextLine();
+            int K, M, N;
+         
+          if (currentLine == 1) {
+            parseInt()
+          } else if (currentLine < 8) {
+            
+          }
+            // Integer arrays for the patchLocations, patchTastiness, cowLocations
+            int[] patchLocs = {0, 4, 8, 10, 12, 13}; // (Left column of inputs below the first line)
+      }
+        
+
+        scanner.close();
+    }
+
+    void solutionMethod() { // may be a separate (probably will be) a different return type instead of void 
+        // Solution work here 
+      }
+}
--- hoofball.py
+++ hoofball.py
@@ -2,6 +2,8 @@ import math
 
 N = int(input())
 places = list(input())
+passed_to = []
+cnt = 0
 
 def target(x):
     left_num = 0
@@ -12,12 +14,32 @@ def target(x):
         if(places[i] < places[x] and places[x] - places[i] < distance_left):
             left_num = i
             distance_left = places[x] - places[i]
-    for i in range(N):
-        if(places[i] > places[x] and places[i] - places[x] < distance_right):
+        elif(places[i] > places[x] and places[i] - places[x] < distance_right):
             right_num = i
             distance_right = places[i] - places[x]
     
     if(distance_right < distance_left):
-        return distance_right
+        return right_num
     else:
-        return distance_left
+        return left_num
+
+
+for i in range(N):
+    target(places[i]).append(passed_to)
+
+for i in range(N):
+    if(places[i] not in passed_to):
+        cnt += 1
+
+
+
+
+
+
+if N == 1 or N == 2:
+    print(1)
+
+
+
+
+
